fe22f3eed61d09c1078a99b7b22dcc67818189f9
Etienne Homer <etiennehomer@gmail.com>
2022-09-23 15:44:12 +0200

    clean
    Signed-off-by: Etienne Homer <etiennehomer@gmail.com>

--- src/main/java/org/gridsuite/useradmin/server/RestResponseEntityExceptionHandler.java
+++ src/main/java/org/gridsuite/useradmin/server/RestResponseEntityExceptionHandler.java
@@ -11,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.server.ServerWebInputException;
 
 import static org.gridsuite.useradmin.server.UserAdminException.Type.FORBIDDEN;
 
