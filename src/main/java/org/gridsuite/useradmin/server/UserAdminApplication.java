246015cfc0eb1940c13cefba1f384064456edba8
sBouzols <sylvain.bouzols@gmail.com>
2023-11-22 16:57:29 +0100

    update to gridsuite dependencies 28
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/main/java/org/gridsuite/useradmin/server/UserAdminApplication.java
+++ src/main/java/org/gridsuite/useradmin/server/UserAdminApplication.java
@@ -6,7 +6,6 @@
  */
 package org.gridsuite.useradmin.server;
 
-import com.powsybl.ws.commons.Utils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -20,7 +19,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class UserAdminApplication {
 
     public static void main(String[] args) {
-        Utils.initProperties();
         SpringApplication.run(UserAdminApplication.class, args);
     }
 }
