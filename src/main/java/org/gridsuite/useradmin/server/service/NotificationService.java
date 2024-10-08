e4352ac39b6ae180012cb8f770e29fd1beb2ebbb
Hugo Marcellin <hugo.marcelin@rte-france.com>
2024-10-08 11:53:31 +0200

    Add cases alert threshold and ability to send message to directory notification server

--- src/main/java/org/gridsuite/useradmin/server/service/NotificationService.java
+++ src/main/java/org/gridsuite/useradmin/server/service/NotificationService.java
@@ -14,11 +14,17 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
+import static com.powsybl.ws.commons.computation.service.NotificationService.HEADER_USER_ID;
+
 /**
  * @author Abdelsalem Hedhili <abdelsalem.hedhili at rte-france.com
  */
 @Service
 public class NotificationService {
+
+    public static final String GLOBAL_CONFIG_BINDING = "publishMessage-out-0";
+
+    public static final String DIRECTORY_UPDATE_BINDING = "publishDirectoryUpdate-out-0";
 
     public static final String HEADER_MESSAGE_TYPE = "messageType";
 
@@ -27,6 +33,14 @@ public class NotificationService {
     public static final String MESSAGE_TYPE_CANCEL_MAINTENANCE = "cancelMaintenance";
 
     public static final String HEADER_DURATION = "duration";
+
+    static final String MESSAGE_TYPE_USER_MESSAGE = "userMessage";
+
+    static final String USER_MESSAGE_VALUES = "messageValues";
+
+    static final String HEADER_UPDATE_TYPE = "updateType";
+
+    static final String HEADER_UPDATE_TYPE_DIRECTORY = "directories";
 
     public static final String MESSAGE_LOG = "Sending message : {}";
 
@@ -37,27 +51,35 @@ public class NotificationService {
     @Autowired
     private StreamBridge updatePublisher;
 
-    private void sendMessage(Message<String> message) {
+    private void sendMessage(Message<String> message, String bindingName) {
         MESSAGE_OUTPUT_LOGGER.debug(MESSAGE_LOG, message);
-        updatePublisher.send("publishMessage-out-0", message);
+        updatePublisher.send(bindingName, message);
     }
 
     public void emitMaintenanceMessage(String message, int duration) {
         sendMessage(MessageBuilder.withPayload(message)
-                .setHeader(HEADER_MESSAGE_TYPE, MESSAGE_TYPE_MAINTENANCE)
-                .setHeader(HEADER_DURATION, duration)
-                .build());
+            .setHeader(HEADER_MESSAGE_TYPE, MESSAGE_TYPE_MAINTENANCE)
+            .setHeader(HEADER_DURATION, duration)
+            .build(), GLOBAL_CONFIG_BINDING);
     }
 
     public void emitMaintenanceMessage(String message) {
         sendMessage(MessageBuilder.withPayload(message)
-                .setHeader(HEADER_MESSAGE_TYPE, MESSAGE_TYPE_MAINTENANCE)
-                .build());
+            .setHeader(HEADER_MESSAGE_TYPE, MESSAGE_TYPE_MAINTENANCE)
+            .build(), GLOBAL_CONFIG_BINDING);
     }
 
     public void emitCancelMaintenanceMessage() {
         sendMessage(MessageBuilder.withPayload("")
-                .setHeader(HEADER_MESSAGE_TYPE, MESSAGE_TYPE_CANCEL_MAINTENANCE)
-                .build());
+            .setHeader(HEADER_MESSAGE_TYPE, MESSAGE_TYPE_CANCEL_MAINTENANCE)
+            .build(), GLOBAL_CONFIG_BINDING);
+    }
+
+    public void emitUserMessage(String sub, String messageId, String messageValues) {
+        sendMessage(MessageBuilder.withPayload(messageValues)
+            .setHeader(MESSAGE_TYPE_USER_MESSAGE, messageId)
+            .setHeader(HEADER_UPDATE_TYPE, HEADER_UPDATE_TYPE_DIRECTORY)
+            .setHeader(HEADER_USER_ID, sub)
+            .build(), DIRECTORY_UPDATE_BINDING);
     }
 }
