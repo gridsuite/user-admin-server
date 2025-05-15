51372881c6e448820f4c0550363b9fe01954540e
Abdelsalem <46495975+AbdelHedhili@users.noreply.github.com>
2025-05-15 13:41:25 +0200

    Add announcements (#68)
    Signed-off-by: Abdelsalem <abdelsalem.hedhili@rte-france.com>
    Co-authored-by: Abdelsalem <abdelsalem.hedhili@rte-france.com>
    Co-authored-by: Joris Mancini <joris.mancini_externe@rte-france.com>
    Signed-off-by: Tristan Chuine <tristan.chuine_externe@rte-france.com>
    Co-authored-by: Tristan Chuine <tristan.chuine_externe@rte-france.com>

--- src/main/java/org/gridsuite/useradmin/server/service/NotificationService.java
+++ src/main/java/org/gridsuite/useradmin/server/service/NotificationService.java
@@ -6,9 +6,10 @@
  */
 package org.gridsuite.useradmin.server.service;
 
+import lombok.AllArgsConstructor;
+import org.gridsuite.useradmin.server.dto.Announcement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
@@ -18,50 +19,39 @@ import org.springframework.stereotype.Service;
  * @author Abdelsalem Hedhili <abdelsalem.hedhili at rte-france.com
  */
 @Service
+@AllArgsConstructor
 public class NotificationService {
-
-    public static final String GLOBAL_CONFIG_BINDING = "publishMessage-out-0";
+    public static final String MESSAGE_TYPE_ANNOUNCEMENT = "announcement";
+    public static final String MESSAGE_TYPE_CANCEL_ANNOUNCEMENT = "cancelAnnouncement";
 
     public static final String HEADER_MESSAGE_TYPE = "messageType";
-
-    public static final String MESSAGE_TYPE_MAINTENANCE = "maintenance";
-
-    public static final String MESSAGE_TYPE_CANCEL_MAINTENANCE = "cancelMaintenance";
-
     public static final String HEADER_DURATION = "duration";
+    public static final String HEADER_ANNOUNCEMENT_ID = "announcementId";
+    public static final String HEADER_SEVERITY = "severity";
 
-    public static final String HEADER_USER_MESSAGE = "userMessage";
-
-    public static final String MESSAGE_LOG = "Sending message : {}";
-
+    public static final String GLOBAL_CONFIG_BINDING = "publishMessage-out-0";
     private static final String CATEGORY_BROKER_OUTPUT = UserAdminService.class.getName() + ".output-broker-messages";
-
     private static final Logger MESSAGE_OUTPUT_LOGGER = LoggerFactory.getLogger(CATEGORY_BROKER_OUTPUT);
 
-    @Autowired
-    private StreamBridge updatePublisher;
+    private final StreamBridge updatePublisher;
 
     private void sendMessage(Message<String> message, String bindingName) {
-        MESSAGE_OUTPUT_LOGGER.debug(MESSAGE_LOG, message);
+        MESSAGE_OUTPUT_LOGGER.debug("Sending message: {}", message);
         updatePublisher.send(bindingName, message);
     }
 
-    public void emitMaintenanceMessage(String message, int duration) {
-        sendMessage(MessageBuilder.withPayload(message)
-            .setHeader(HEADER_MESSAGE_TYPE, MESSAGE_TYPE_MAINTENANCE)
-            .setHeader(HEADER_DURATION, duration)
+    public void emitAnnouncementMessage(Announcement announcement) {
+        sendMessage(MessageBuilder.withPayload(announcement.message())
+            .setHeader(HEADER_MESSAGE_TYPE, MESSAGE_TYPE_ANNOUNCEMENT)
+            .setHeader(HEADER_ANNOUNCEMENT_ID, announcement.id())
+            .setHeader(HEADER_DURATION, announcement.remainingTimeMs())
+            .setHeader(HEADER_SEVERITY, announcement.severity())
             .build(), GLOBAL_CONFIG_BINDING);
     }
 
-    public void emitMaintenanceMessage(String message) {
-        sendMessage(MessageBuilder.withPayload(message)
-            .setHeader(HEADER_MESSAGE_TYPE, MESSAGE_TYPE_MAINTENANCE)
-            .build(), GLOBAL_CONFIG_BINDING);
-    }
-
-    public void emitCancelMaintenanceMessage() {
+    public void emitCancelAnnouncementMessage() {
         sendMessage(MessageBuilder.withPayload("")
-            .setHeader(HEADER_MESSAGE_TYPE, MESSAGE_TYPE_CANCEL_MAINTENANCE)
+            .setHeader(HEADER_MESSAGE_TYPE, MESSAGE_TYPE_CANCEL_ANNOUNCEMENT)
             .build(), GLOBAL_CONFIG_BINDING);
     }
 }
